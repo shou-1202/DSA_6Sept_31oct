20bdd3446c2c3e3f8d7c15b8677f1f133707d4b5
shou-1202 <shouryagarg1202@gmail.com>
2026-09-15 19:34:33 +0530

    Add files via upload

---
+++ 207. Course Schedule/Solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>indegree(numCourses, 0);
+        vector<int>adj[numCourses];
+        for(int i = 0; i<prerequisites.size(); i++){
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        
+        for(int i = 0; i<prerequisites.size(); i++){
+            int a = prerequisites[i][1];
+            indegree[a] += 1;
+        }
+        queue<int>q;
+        for(int i = 0; i<numCourses; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int n = q.front();q.pop();
+            topo.push_back(n);
+            for(auto neigh: adj[n]){
+                indegree[neigh]--;
+                if(indegree[neigh] == 0){
+                    q.push(neigh);
+                }
+            }
+        }
+        return topo.size() == numCourses? true: false;
+    }
+};
